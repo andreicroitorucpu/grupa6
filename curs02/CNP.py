b13e32135be3832989dc5e34621cd111ce7f753b
Andrei Croitoru <croyandrei@gmail.com>
2022-12-06 18:44:02 +0200

    tema02

--- curs02/CNP.py
+++ curs02/CNP.py
@@ -2,7 +2,7 @@ cnp = "279146358279"
 
 
 def county_check(cnp):
-    counties = ["%.2d" % i for i in range(1,47)]
+    counties = ["%.2d" % i for i in range(1, 47)]
     counties.append('51')
     counties.append('52')
     inserted_county = cnp[6:8]
